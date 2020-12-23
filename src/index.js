783916ccd0cf19c44623ecdb92e750209b2bf857
David Strauch <dmaster18@gmail.com>
2020-12-23 20:48:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // write your createStore function here
 
-
+function createStore(reducer) {
+  
+  
+  
+}
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
